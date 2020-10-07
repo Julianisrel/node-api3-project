82c55fb1578382b0e5171b053ae6d64d8c9ddb13
Julian Cole <juliancole@Julians-MacBook-Pro.local>
2020-10-06 19:21:09 -0700

    starting

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code away!
+const express = require("express")
+// const morgan = require("morgan")
+const deny = require("./middleware/deny")
+const logger = require("./middleware/logger")
+const welcomeRouter = require("./welcome/welcome-router")
+const usersRouter = require("./users/users-router")
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+// server.use(morgan("combined"))
+// server.use(deny())
+server.use(logger("long"))
+
+server.use(welcomeRouter)
+server.use(usersRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong, please try again later",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
